4a2d6b7e9d613d749b2833242275018a4180467d
Tiffany Du <tadofdesign@gmail.com>
2017-06-09 16:14:14 -0500

    Created app.jsfile and created a new Vue instance

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+  // Target div#events
+  el: '#events',
+  // Register any values or collections that hold data for the application here
+  data: {},
+  // Anything within the ready function will run when the application loads
+  ready: function() {},
+  // Methods we want to use in our application are registered here
+  methods: {}
+});
