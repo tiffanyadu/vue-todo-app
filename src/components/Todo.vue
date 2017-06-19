673cbd290700197082ebc8a3d3ba692885e84c88
Tiffany Du <tadofdesign@gmail.com>
2017-06-19 14:38:53 -0500

    Added trash icon and methods to delete todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -11,6 +11,9 @@
         <span class="right floated edit icon" v-on:click="showForm">
           <i class="edit icon"></i>
         </span>
+        <span class="right floated trash icon" v-on:click="deleteTodo(todo)">
+          <i class="trash icon"></i>
+        </span>
       </div>
     </div>
     <div class="content" v-show="isEditing">
@@ -53,6 +56,9 @@
       },
       hideForm() {
         this.isEditing = false;
+      },
+      deleteTodo(todo) {
+        this.$emit('delete-todo', todo);
       },
     },
   };
