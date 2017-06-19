d8dd1aaf24f9448e4af3ef402500b5a5e422c364
Tiffany Du <tadofdesign@gmail.com>
2017-06-19 14:05:53 -0500

    Added component data and rendered data to page using Vue's v-for loop

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -8,8 +8,31 @@
 import TodoList from './components/TodoList';
 
 export default {
+  name: 'app',
   components: {
     TodoList,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
